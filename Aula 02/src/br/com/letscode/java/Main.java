6172852cf6ba4f78d6d0be04058202bc65b56f3f
Fabio Costa <fabio.costta@yahoo.com.br>
2022-09-14 23:55:03 -0300

    configuracaoInicial e testes

---
+++ Aula 02/src/br/com/letscode/java/Main.java
@@ -0,0 +1,9 @@
+package br.com.letscode.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello World!!!");
+    }
+}
